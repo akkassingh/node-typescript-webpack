d168cb30019264b70763f300bff792b1ec08b88b
akkas <akkassingh@gmail.com>
2022-04-23 18:07:31 +0530

    creating server

--- src/Server/Server.ts
+++ src/Server/Server.ts
@@ -1,6 +1,14 @@
+import { createServer } from 'http';
+
 export class Server {
 
     public createServer(){
-        console.log('created server');
+        createServer(
+            (req, res) => {
+                console.log("got req from: ", req.url);
+                res.end();
+        }
+        ).listen(8080)
+        console.log("server started")
     }
 }
